971821701b6e5a6cd863bd17a8998795c6c35c24
Leslie Carr <geekgirl@gmail.com>
2014-04-14 15:45:16 -0700

    expanding configuration file creation

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,7 @@
 # NOTE: uncertain whether or not to have configuration file or everything parameters
 class quagga::params {
 
+  # running zebra is highly recommended
   $zebra  = true,
   $bgpd   = false,
   $ospfd  = false,
@@ -9,13 +10,28 @@ class quagga::params {
   $ripd   = false,
   $ripngd = false,
   $isisd  = false,
-  $babeld = false)
+  $babeld = false
+  #do not publish password in public repositories
+  $password = ''
   $router_id = $::network_lo
+
+  #BGP variables
   $AS = ''
-  #$bgp_interfaces should be a list
-  $bgp_interfaces = ''
-  #$ospf_interfaces should be a list
-  $ospf_interfaces = ''
-  #$ospf_area should be in dotted quad format
-  $ospf_area = '0.0.0.0'
+  $bgp_alwayscomparemed = false
+  $bgp_logfile = '/var/log/quagga/bgpd.log'
+  #$bgp_neighbors is an array in the format 
+  # ISP1 => { 'neighbor_ip' => '192.0.2.1', 'neighbor_as' => '65001' }
+  $bgp_neighbors = {
+    neighbor1 => { 'neighbor_ip' => '', 'neighbor_as' => '' },
+  }
+
+  #OSPF variables
+  $ospf_logfile = '/var/log/quagga/ospfd.log'
+  #$ospf_interfaces is an array in the format 
+  $ospf_interfaces = {
+    interface1 => { 'interface' => '', 'area' => ''},
+  }
+
+  #ospf6_interfaces is an array. By default assign it the same values as ospf_interfaces
+  $ospf6_interfaces = $ospf_interfaces
 }
