0293f8dcdd476f63dd6a9a7ef7f2fc0d383bb6d8
edpto <tor.ladre@it.uib.no>
2016-05-10 10:40:00 +0200

    Enable overridable parameters

--- manifests/bgpd.pp
+++ manifests/bgpd.pp
@@ -1,12 +1,19 @@
 #Class quagga::bgpd
 
-class quagga::bgpd {
+class quagga::bgpd (
+  $bgp_hostname   = $quagga::params::bgp_hostname
+  $bgp_password   = $quagga::params::bgp_password
+  $bgp_logfile    = $quagga::params::bgp_logfile
+  $bgp_as         = $quagga::params::bgp_as
+  $bgp_comparemed = $quagga::params::bgp_comparemed
+  $bgp_neighbors  = $quagga::params::bgp_neighbors
+) {
 
-    file { '/etc/quagga/bgpd.conf':
-      mode    => '0644',
-      owner   => 'quagga',
-      group   => 'quagga',
-      content => template('quagga/bgpd.conf.erb'),
-      notify  => Service['quagga'],
-    }
+  file { '/etc/quagga/bgpd.conf':
+    mode    => '0644',
+    owner   => 'quagga',
+    group   => 'quagga',
+    content => template('quagga/bgpd.conf.erb'),
+    notify  => Service['quagga'],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,34 +2,40 @@
 # NOTE: uncertain whether or not to have configuration file or everything parameters
 class quagga::params {
 
+  $manage_package = true
+  $manage_service = true
   # running zebra is highly recommended
-  $zebra  = true,
-  $bgpd   = false,
-  $ospfd  = false,
-  $ospf6d = false,
-  $ripd   = false,
-  $ripngd = false,
-  $isisd  = false,
+  $zebra  = true
+  $bgpd   = false
+  $ospfd  = false
+  $ospf6d = false
+  $ripd   = false
+  $ripngd = false
+  $isisd  = false
   $babeld = false
   #do not publish password in public repositories
-  $password = ''
+  $password = 'pass123'
   $router_id = $::network_lo
 
   #BGP variables
-  $AS = ''
+  $bgp_hostname = 'bgpd'
+  $bgp_password = 'cn321'
+  $bgp_enable_password = 'cn321'
+  $bgp_bgp_comparemed = false
+  $bgp_as = '65001'
   $bgp_alwayscomparemed = false
   $bgp_logfile = '/var/log/quagga/bgpd.log'
   #$bgp_neighbors is an array in the format 
   # ISP1 => { 'neighbor_ip' => '192.0.2.1', 'neighbor_as' => '65001' }
   $bgp_neighbors = {
-    neighbor1 => { 'neighbor_ip' => '', 'neighbor_as' => '' },
+    neighbor1 => { 'neighbor_ip' => '192.0.2.1', 'neighbor_as' => '65001' },
   }
 
   #OSPF variables
   $ospf_logfile = '/var/log/quagga/ospfd.log'
   #$ospf_interfaces is an array in the format 
   $ospf_interfaces = {
-    interface1 => { 'interface' => '', 'area' => ''},
+    interface1 => { 'interface' => '192.0.2.1/24', 'area' => '0'},
   }
 
   #ospf6_interfaces is an array. By default assign it the same values as ospf_interfaces
